30db6ca791e7c9a16c7de43a2e0af26eb1f4cec4
Sergey Klimov <s.klimov@bk.ru>
2021-07-08 15:04:45 +0700

    Оптимизировал код обработки параметров комнадной строки

--- postgres_to_es/app.py
+++ postgres_to_es/app.py
@@ -6,79 +6,50 @@ from time import sleep
 import elasticsearch
 import psycopg2
 import redis
-from pydantic import BaseModel, validator, ValidationError
 
 from handler import movie_etl, serial_etl, genre_etl, person_etl, moviepersonrole_etl, serialpersonrole_etl
 
 
-class Loop(BaseModel):
+def start(batch_size: int, interval: int, test_pass: int):
     """
-    Класс бесконечного цикла поиска изменений и запуска ETL-процессов.
+    Запускаем бесконечный цикл поиска изменений и запуска ETL-процессов.
     :batch_size: размер пачки данных, чтобы не перегружать сервисы
     :interval: интервал в секундах, через который запускается каждый новый цикл
     :test_pass: если данный параметр указан, то цикл остановится через указанное в нем кол-во повторений
+    :return:
     """
-    batch_size: int
-    interval: int
-    test_pass: int
-    
-    @validator('batch_size')
-    def validate_age(cls, value):
-        if int(value) < 1:
-            raise ValueError('the parameter batch_size should be greater than 0')
-        return str(value)
-    
-    @validator('interval')
-    def validate_interval(cls, value):
-        if int(value) < 1:
-            raise ValueError('the parameter interval should be greater than 0')
-        return str(value)
-    
-    @validator('test_pass')
-    def validate_test_pass(cls, value):
-        if int(value) < 0:
-            raise ValueError('the parameter test_pass should be greater than or equal 0')
-        return str(value)
-    
-    def start(self):
-        """
-        Запускаем бесконечный цикл поиска изменений и запуска ETL-процессов.
-        :return:
-        """
-        counter = 0
-        while True:
-            logging.info(f'Start loop at {datetime.now()}')
+    counter = 0
+    while True:
+        logging.info(f'Start loop at {datetime.now()}')
         
-            try:
-                movie_etl(batch_size)
-                serial_etl(batch_size)
-                genre_etl(batch_size)
-                person_etl(batch_size)
-                moviepersonrole_etl(batch_size)
-                serialpersonrole_etl(batch_size)
-            except psycopg2.OperationalError as e:
-                pass
-            except elasticsearch.exceptions.ConnectionError as e:
-                pass
-            except redis.exceptions.ConnectionError as e:
-                pass
+        try:
+            movie_etl(batch_size)
+            serial_etl(batch_size)
+            genre_etl(batch_size)
+            person_etl(batch_size)
+            moviepersonrole_etl(batch_size)
+            serialpersonrole_etl(batch_size)
+        except (psycopg2.OperationalError,
+                elasticsearch.exceptions.ConnectionError,
+                redis.exceptions.ConnectionError):
+            pass
         
-            logging.info(f'Pause {interval} seconds to the next ETL processes')
-            sleep(interval)
+        logging.info(f'Pause {interval} seconds to the next ETL processes')
+        sleep(interval)
         
-            # Если мы в режиме отладки, считаем количество итераций
-            if test_pass:
-                counter += 1
-            if test_pass and counter == test_pass:
-                break
+        # Если мы в режиме отладки, считаем количество итераций
+        if test_pass:
+            counter += 1
+        if test_pass and counter == test_pass:
+            break
 
 
 def get_args():
     """
     Получаем аргументы командной строки.
-    :return batch_size: размер "пачки", перегружаемых за один раз.
-    :return interval: период в секундах, через который запускаются процессы перегрузки.
-    :return test_pass: число итераций скрипта. Использовать для процесса отдладки.
+    :return batch_size: размер пачки данных, чтобы не перегружать сервисы
+    :return interval: интервал в секундах, через который запускается каждый новый цикл
+    :return test_pass: если данный параметр указан, то цикл остановится через указанное в нем кол-во повторений
     """
     parser = argparse.ArgumentParser(description='ETL process parameters')
     parser.add_argument("--batch_size", type=int, default=5, help="Batch size to load in Elastic Search")
@@ -86,21 +57,26 @@ def get_args():
     parser.add_argument("--test_pass", type=int, default=0, help="Number of test passes")
     
     args = parser.parse_args()
+    if args.batch_size < 1:
+        raise ValueError('the parameter batch_size should be greater than 0')
+    if args.interval < 1:
+        raise ValueError('the parameter interval should be greater than 0')
+    if args.test_pass < 0:
+        raise ValueError('the parameter test_pass should be greater than or equal 0')
+    
     return args.batch_size, args.interval, args.test_pass
 
 
 if __name__ == '__main__':
-   
+    
     logging.basicConfig(level=logging.INFO, filename='log.txt', filemode='w',
                         format='%(levelname)s - %(asctime)s - %(name)s - %(message)s',
                         datefmt='%d-%b-%y %H:%M:%S')
     logging.getLogger('backoff').addHandler(logging.StreamHandler())
 
-    batch_size, interval, test_pass = get_args()  # Получаем аргументы командной строки
-
     try:
-        loop = Loop(batch_size=batch_size, interval=interval, test_pass=test_pass)
-    except ValidationError as e:
-        logging.error(f'Exception {e.json()}')
+        batch_size, interval, test_pass = get_args()  # Получаем аргументы командной строки
+    except ValueError as e:
+        logging.error(f'Exception {e}')
     else:
-        loop.start()
+        start(batch_size, interval, test_pass)
